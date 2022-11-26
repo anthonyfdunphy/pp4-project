b92aacf99f78fef14772d737b9e1e37df7bbd0e8
anthonyfdunphy <anthonyfdunphy@gmail.com>
2022-11-26 16:25:07 +0000

    test.py edits

--- mysite/blog/tests.py
+++ mysite/blog/tests.py
@@ -9,5 +9,6 @@ class TryDjangoConfigTest(TestCase):
         try:
             is_strong = validate_password(SECRET_KEY)
         except Exception as e:
-            self.fail(e)   
+            msg = f'Weak Secret Key {e.messages}'
+            self.fail(msg)   
 
